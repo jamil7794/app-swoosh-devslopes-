ee44d1997881c990862141a1c1e36b0f5cc07654
JamilJalal <jamil.jalal94@gmail.com>
2018-02-05 17:15:25 -0500

    Added files and changed for auto layout

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Jamil Jalal on 2/4/18.
+//  Copyright © 2018 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        // Setting up frame for all devices
+        super.viewDidLoad()
+        /*swoosh.frame = CGRect(x: view.frame.size.width/2-swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width,height: swoosh.frame.size.height)
+        bgImg.frame = view.frame*/
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
