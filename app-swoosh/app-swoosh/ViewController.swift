c93f9af9f27c6e8ca102d2cc5b7d12b5de334e21
JamilJalal <jamil.jalal94@gmail.com>
2018-02-05 17:27:30 -0500

    No changes

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         // Setting up frame for all devices
         super.viewDidLoad()
-        swoosh.frame = CGRect(x: view.frame.size.width/2-swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width,height: swoosh.frame.size.height)
-        bgImg.frame = view.frame
+        /*swoosh.frame = CGRect(x: view.frame.size.width/2-swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width,height: swoosh.frame.size.height)
+        bgImg.frame = view.frame*/
         
     }
 
