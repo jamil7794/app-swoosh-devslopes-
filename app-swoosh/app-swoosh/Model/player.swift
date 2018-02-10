84fc6b4bf31ffbbd1f8a3a460d09d001e9496e93
JamilJalal <jamil.jalal94@gmail.com>
2018-02-10 03:09:31 -0500

    Mistake 2

---
+++ app-swoosh/app-swoosh/Model/player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  app-swoosh
+//
+//  Created by Jamil Jalal on 2/10/18.
+//  Copyright © 2018 Jamil Jalal. All rights reserved.
+//
+
+import Foundation
+
+struct player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
