08968fea1c50883803cc9a34036b026d4771a5fd
JamilJalal <jamil.jalal94@gmail.com>
2018-02-10 03:50:34 -0500

    Mistake solved for febb 2018

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var p: player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        p = player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+       selectLeague(leagueType :"Men")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType :"Women")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "Coed")
+    }
+    
+    func selectLeague(leagueType :String){
+        p.desiredLeague=leagueType
+        nextBtn.isEnabled=true
     }
     
 }
